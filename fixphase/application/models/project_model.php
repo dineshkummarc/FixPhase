25b18766ac8700d43bd2b2c7fcac779cadccc441
Abdulaziz Alaa <abdulazizalaa@ymail.com>
2015-04-21 17:33:52 +0200

    Manipulated the Project controller
    added view function to the project controller
    added get_all_projects function to the project_model model
    added get_project function to the project_model model

---
+++ fixphase/application/models/project_model.php
@@ -0,0 +1,27 @@
+<?php
+
+
+  class project_model extends CI_Model{
+
+
+    //get all projects in database
+    //with
+    //offset $offset
+    //limit $limit
+    public function get_all_projects($offset, $limit){
+      $this->db->select('project_id, project_name');
+      $query = $this->db->get("project", $offset, $limit);
+      return $query->result();
+    }
+
+    //get the data of a specific project with project_id = $id
+    public function get_project($id){
+      //leave until the database is updated with new attributes for the project data
+      //$this->db->select('project_id, project_name');
+      $this->db->where('project_id', $id);
+      $query = $this->db->get("project");
+      return $query->result();
+    }
+
+
+  }
