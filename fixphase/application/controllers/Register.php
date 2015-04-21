9f117c6ec99c4839529546be1af43ca78bdd9722
Abdulaziz Alaa <abdulazizalaa@ymail.com>
2015-04-21 13:23:20 +0200

    Added Controllers

--- fixphase/application/controllers/Register.php
+++ fixphase/application/controllers/Register.php
@@ -3,5 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller{
 
-  
+
+  //use it to load the view of register to the user when the url /Register is loaded
+  public function index(){
+
+  }
+  //use this to validate the input from the user
+  //Either the input is okay make a function to insert the user data in database
+  //or return an error
+  public function validate(){
+
+  }
+
+
 }
