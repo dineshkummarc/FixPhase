13a14c02bbbe6cfeb9b1be6c28fd6a681999f39a
Bassemtarekx <bassemtarekx@gmail.com>
2015-04-21 18:06:42 +0200

    Revert "Registration controller"
    This reverts commit 132f9f8693af46eb30d048035571e74c97645088.

--- fixphase/application/models/User.php
+++ fixphase/application/models/User.php
@@ -1,28 +1,5 @@
 <?php
 
 class User extends CI_Model{
- public function insert_user()
- {
-	 $fname= strtolower($this->input->post('Fname'));
-	 $lname= strtolower($this->input->post('Lname'));
-	 $data = array(
-	 'username'=> strtolower($this->input->post('username')),
-	 'password'=> strtolower($this->input->post('password')),
-	 
-	 'email'=> strtolower($this->input->post('email')),	
-     'full_name'=> $fname . " " . $lname
-	 );
-	 
-	 
-	 $query = $this->db->insert('users',$data);
-	 if($query)
-	 {
-		 echo "3aab 3alaak";
-	 }
-     	 else
-		 {
-			 echo "bdan naak";
-		 }
-	 
- } 
+  
 }
