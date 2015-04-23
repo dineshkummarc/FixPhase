21d7050d63947761de2f30f716b857ece07c1e80
Omar Essam <omar_2412@me.com>
2015-04-23 16:28:24 +0200

    Moataz's comments

--- fixphase/application/views/Login_view.php
+++ fixphase/application/views/Login_view.php
@@ -1,3 +1,4 @@
+<?php echo validation_errors(); ?>
 <form method="POST" action="http://localhost/FixPhase/fixphase/Login/validate">
      <input type="text" name="user_email">
      <input type="password" name="password">
