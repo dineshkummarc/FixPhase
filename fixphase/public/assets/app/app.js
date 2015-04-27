b163484cdb8e747029272db542223b4c3391bc17
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-04-27 09:42:19 +0300

    Basic Configurations

--- fixphase/public/assets/app/app.js
+++ fixphase/public/assets/app/app.js
@@ -1,1 +1,13 @@
+requirejs.config({
+    baseUrl: '/assets/app',
+    paths: {
+        jquery: "../lib/jquery/jquery",
+        router: "../lib/requirejs-router/router",
+        text: "../lib/requirejs-text/text"
+    }
+});
 
+
+require(["router","config/routes"], function(router,routes){
+
+});
