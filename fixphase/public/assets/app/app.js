8a8ec754c3ba0e6a77227b7601a577e774f7b75c
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-04-27 10:19:47 +0300

    Adding require-css.

--- fixphase/public/assets/app/app.js
+++ fixphase/public/assets/app/app.js
@@ -3,11 +3,11 @@ requirejs.config({
     paths: {
         jquery: "../lib/jquery/jquery",
         router: "../lib/requirejs-router/router",
-        text: "../lib/requirejs-text/text"
+        text: "../lib/requirejs-text/text",
+        css : "../lib/require-css/css"
     }
 });
 
 
 require(["router","config/routes"], function(router,routes){
-
 });
