50360ac37ebd051af4cd953e9eb5948fc6577bdf
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-04-28 17:47:13 +0300

    Fixing main app routes order.

--- fixphase/public/assets/app/config/routes.js
+++ fixphase/public/assets/app/config/routes.js
@@ -4,12 +4,12 @@ define({
 
     info: { path: '/project/:pid', moduleId: 'pages/projectInfo/projectInfo' },
 
-    defects: { path: '/project/:pid/defects*', moduleId: 'pages/defects/defects' },
+    defects: { path: '/project/:pid/defects', moduleId: 'pages/defects/defects' },
 
-
-    defectView: { path: '/project/:pid/defect/:did', moduleId: 'pages/defectView/defectView' },
 
     defectCreate: { path: '/project/:pid/defect/create', moduleId: 'pages/defectCreate/defectCreate' },
+
+    defectView: { path: '/project/:pid/defect/:did', moduleId: 'pages/defectView/defectView' },
 
     graphs: { path: '/project/:pid/graphs', moduleId: 'pages/graphs/graphs' },
 
