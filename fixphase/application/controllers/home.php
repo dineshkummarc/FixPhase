9dba120a3b7daeb0494b4b96d186778e91f83354
Abdulaziz Alaa <abdulazizalaa@ymail.com>
2015-04-28 08:59:21 +0200

    Adjusted Home Controller to work with rest
    Sorry for making this commit on the master

--- fixphase/application/controllers/home.php
+++ fixphase/application/controllers/home.php
@@ -5,26 +5,27 @@ require_once APPPATH.'/libraries/Auth_Controller.php';
 
 class Home extends Auth_Controller {
 
-	public function index_get(){
-		//GET requests
-		echo "get";
-		$this->index();
-	}
+    public function index_get(){
+        //GET requests
+        echo "get";
+        $this->load->view('home_view');
+    }
 
-	public function index_post(){
-		//POST requests
-		echo "post";
-	}
+    public function index_post(){
+        //POST requests
+        echo "post";
+    }
 
-	public function index_delete(){
-		//DELETE requests
-		echo "delete";
-	}
+    public function index_delete(){
+        //DELETE requests
+        echo "delete";
+    }
 
-	public function index_put(){
-		//PUT requests
-		echo "put";
-	}
+    public function index_put(){
+        //PUT requests
+        echo "put";
+    }
+
 	public function index()
 	{
 		$this->load->view('home_view');
