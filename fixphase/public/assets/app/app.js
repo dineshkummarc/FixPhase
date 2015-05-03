be863fbf1a74cc05ad30d0cfea92564afc4d42e0
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-05-03 10:12:17 +0200

    Updating front end to use custom framework.

--- fixphase/public/assets/app/app.js
+++ fixphase/public/assets/app/app.js
@@ -2,13 +2,33 @@ requirejs.config({
     baseUrl: '/assets/app',
     paths: {
         jquery: "../lib/jquery/jquery",
-        router: "../lib/requirejs-router/router",
         text: "../lib/requirejs-text/text",
-        css : "../lib/require-css/css"
+        css : "../lib/require-css/css",
+
+        identity: "../lib/core/identity",
+        core: "../lib/core/core",
+        router: "../lib/core/router",
+        events: "../lib/core/events",
+        observer: "../lib/core/observer",
+        component: "../lib/core/component",
+        skeleton: "../lib/core/skeleton",
+        gui: "../lib/core/gui",
+        page: "../lib/core/page",
+        widget: "../lib/core/widget",
+
+        store: "../lib/core/store"
+
     }
 });
 
 
-require(["router","config/routes"], function(router,routes){
+require(["core","router","config/routes"], function(core,router,routes){
+    //Register the routes in the router library
     router.registerRoutes(routes);
+
+    //Set the method that will handle all url change events
+    router.on("routeload", core.onRouteLoad);
+
+    //initialize the router library to fire the first event
+    router.init();
 });
--- fixphase/public/assets/app/config/routes.js
+++ fixphase/public/assets/app/config/routes.js
@@ -14,5 +14,64 @@ define({
     graphs: { path: '/project/:pid/graphs', moduleId: 'pages/graphs/graphs' },
 
     // matches everything else
-    notFound: { path: '*', moduleId: 'page/notFound/notFound' }
+    notFound: { path: '*', moduleId: 'pages/notFound/notFound' }
+});
+
+define({
+    // matches an exact path
+    home: {
+
+        //The url path, the (:..) matches any segment and pass it with the same name as
+        // object property to the argument object passed to the event callback
+        // listening to this url. So in this case 'name' is the property.
+        path: '/',
+
+        //Page module to be loaded on this url
+        page: 'pages/home/home',
+
+        // Skeleton tree defines the order of skeletons that should exist before this page
+        // is loaded, the tree is an array where order defines ancestry, so for example object
+        // at index 2 rquires that object at index 1 be loaded in DOM first, and object at
+        // index 1 requires object at index 0, etc...
+        // Skeleton tree can be empty if the page should be included in the body tag.
+        skeletonTree: ['skeletons/basic/basic']
+    },
+
+    info: {
+        path: '/project/:pid',
+        page: 'pages/projectInfo/projectInfo',
+        skeletonTree: ['skeletons/basic/basic', 'skeletons/nav/nav']
+    },
+
+    defects: {
+        path: '/project/:pid/defects',
+        page: 'pages/defects/defects' ,
+        skeletonTree: ['skeletons/basic/basic', 'skeletons/nav/nav']
+    },
+
+
+    defectCreate: {
+        path: '/project/:pid/defect/create',
+        page: 'pages/defectCreate/defectCreate',
+        skeletonTree: ['skeletons/basic/basic', 'skeletons/nav/nav']
+    },
+
+    defectView: {
+        path: '/project/:pid/defect/:did',
+        page: 'pages/defectView/defectView' ,
+        skeletonTree: ['skeletons/basic/basic', 'skeletons/nav/nav']
+    },
+
+    graphs: {
+        path: '/project/:pid/graphs',
+        page: 'pages/graphs/graphs' ,
+        skeletonTree: ['skeletons/basic/basic', 'skeletons/nav/nav']
+    },
+
+    // matches everything else
+    notFound: {
+        path: '*',
+        page: 'pages/notFound/notFound' ,
+        skeletonTree: ['skeletons/basic/basic']
+    }
 });
