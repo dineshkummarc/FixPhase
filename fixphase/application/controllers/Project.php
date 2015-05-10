d6cc4293e2e5291d4061d4b144cc930a8ca3e281
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-05-10 15:22:53 +0200

    Fix project api

--- fixphase/application/controllers/Project.php
+++ fixphase/application/controllers/Project.php
@@ -10,46 +10,32 @@ class Project extends Auth_Controller{
   }
 
   public function index_get(){
-    //GET requests
-    echo "get";
-    //use this to insure that the request is coming from an ajax call not from a browser
-    //if($this->input->is_ajax_request()){
-      if($this->get('pid') == null){
-        //request to get all the projects in the database
-        //will return ids and names only
-        $this->load->model('project_model');
-        $json = new stdClass();
-        $json->session_id = $this->session->userdata('session_id');
-        $json->data = new stdClass();
-        $json->data->projects = $this->project_model->get_all_projects(0, 500);
-        $json->error = new stdClass();
-        $json->error->status = "false";
-        echo json_encode($json);
+
+    if($this->get('pid') == null){
+      $this->load->model('project_model');
+      $projects = $this->project_model->get_all_projects(0, 500);
+      $this->response(array("data" => $projects));
         /*
         {
-          "session_id":null,
           "data":
-            {
-              "projects":
                 [
                   {"project_id":"1","project_name":"game"},
                   {"project_id":"2","project_name":"hayyy"}
                 ]
-            },
-          "error":{"status":"false"}
+          "error":{"msg": "msg"} //exist only if error
         }
         */
 
       }else{
-        //request to get data about a specific project with the project id
-        $this->load->model('project_model');
-        $json = new stdClass();
-        $json->session_id = $this->session->userdata('session_id');
-        $json->data = new stdClass();
-        $json->data->project = $this->project_model->get_project($this->get('pid'));
-        $json->error = new stdClass();
-        $json->error->status = "false";
-        $json->error->message = "";
+            //request to get data about a specific project with the project id
+            $this->load->model('project_model');
+            $json = new stdClass();
+            $json->session_id = $this->session->userdata('session_id');
+            $json->data = new stdClass();
+            $json->data->project = $this->project_model->get_project($this->get('pid'));
+            $json->error = new stdClass();
+            $json->error->status = "false";
+            $json->error->message = "";
         echo json_encode($json);
         /*
         {
