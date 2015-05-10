eb69fee5cd523806ee258c3567dc7677d14531b0
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-05-10 22:53:41 +0200

    Linked used frontend libraries.

--- fixphase/public/assets/app/app.js
+++ fixphase/public/assets/app/app.js
@@ -1,14 +1,21 @@
 requirejs.config({
     baseUrl: '/assets/app',
+    shim : {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
     paths: {
         jquery: "../lib/jquery/jquery",
+        "jquery-ui": "../lib/jquery-ui/ui",
         text: "../lib/requirejs-text/text",
         css : "../lib/require-css/css",
+        "bootstrap-css":"../lib/bootstrap/css/bootstrap",
+        "bootstrap":"../lib/bootstrap/js/bootstrap",
+        smoothness: "../lib/jquery-ui/themes/smoothness/jquery-ui",
+
 
         //config
         routes: "config/routes",
         api: "config/api",
-
 
         //framework
         identity: "../lib/core/identity",
@@ -29,7 +36,7 @@ requirejs.config({
 });
 
 
-require(["core","router","routes"], function(core,router,routes){
+require(["core","router","routes","bootstrap","css!bootstrap-css"], function(core,router,routes){
     //Register the routes in the router library
     router.registerRoutes(routes);
 
