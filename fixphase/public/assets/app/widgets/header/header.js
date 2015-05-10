84f76151c9df2270457aba10434a8c1d34575a9d
Aya Khaled <aya_94_a2@hotmail.com>
2015-05-10 17:41:10 +0200

    added header with define format

--- fixphase/public/assets/app/widgets/header/header.js
+++ fixphase/public/assets/app/widgets/header/header.js
@@ -1,143 +1,169 @@
-/**
- * Created by Aya on 4/25/2015.
- */
-//define(['jquery','text!widgets/header/header.html!'],function($){
-$(document).ready(function () {
-    var searchResult = $('.search-result');
+define(
+    [
+        "text!widgets/header/header.html!strip",
+        "widget",
+        'jquery',
+        "css!widgets/header/header.css"
+    ],
+    function (html, Widget, $) {
 
-    searchResult.hide();
+        return new Widget({
 
-    var userID = "Aya";
-    var username = $('<a/>',{href:"#"});
-    username.append(userID);
-    $('.username').append(username);
+            //a must have property where content are setup
+            makeContent: function () {
+                var content = $(($.parseHTML($.trim(html)))[0]);
+                //register any DOM events here, like onClick
+                var self = this;
 
-    var dropDownMenu = $('.project-menu');
-    var projectElement = $('.project-element');
+                var username = content.find('.username').text();
 
-    var addProjectBtn = $('.add-project-btn');
-    var addProjectForm = $('.addNewProject');
+                var dropDownMenu = content.find('.project-menu');
+                var projectElement = content.find('.project-element');
 
-    var closeDialog = $('.close-dialog');
-    var searchField = $('.search-box');
+                var addProjectBtn = content.find('.add-project-btn');
+                var addProjectForm = content.find('.addNewProject');
 
-    var inviteForm = $('.invite');
+                var closeDialog = content.find('.close-dialog');
+                var searchField = content.find('.search-box');
 
+                var inviteForm = content.find('.invite');
 
+                var url = "user.json";
 
-    var url = "user.json";
-
-    //invite to project POPUP
-    inviteForm.dialog({
-        autoOpen: false,
-        show: {
-            effect: "fade"
-        },
-        modal: true,
-        draggable: false,
-        fluid: true,
-        resizable: false,
-        hide: {
-            effect: "fade"
-        },
-        Close: function () {
-            $(this).dialog("close");
-        },
-        Continue: function () {
-            $(this).dialog("close");
-        }
-    });
-
-    $.getJSON(url, function (responese) {
-        $.each(responese, function (index, user) {
-            if(user.username==userID){
-                $.each(user.projects, function (projectIndex,projectName) {
-                    //create new list item
-                    var project = $('<li/>');
-                    project.prop("class","project-element");
-
-                    //create new project link
-                    var project_link = $('<a/>',{href:"#"});
-
-                    //create new invite button for each project
-                    var invite_btn = $('<button/>',{type:"button" , class:"add-btn"});
-
-                    invite_btn.click(function () {
-                        inviteForm.dialog("open");
-                    });
-
-                    invite_btn.addClass("btn btn-default");  //new class for styling
-                    invite_btn.text("+");
-
-                    //get project name
-                    var projectN = user.projects[projectIndex].project;
-
-                    //append name of each project to the project link
-                    project_link.text(projectN);
-
-                    //append the project link to the list item then append the invite button
-                    project.append(project_link);
-                    project.append(invite_btn);
-
-                    //append the list item after the search box
-                    $('.project-menu li:eq(1)').after(project);
+                //invite to project POPUP
+                inviteForm.dialog({
+                    autoOpen: false,
+                    show: {
+                        effect: "fade"
+                    },
+                    modal: true,
+                    draggable: false,
+                    fluid: true,
+                    resizable: false,
+                    hide: {
+                        effect: "fade"
+                    },
+                    Close: function () {
+                        $(this).dialog("close");
+                    },
+                    Continue: function () {
+                        $(this).dialog("close");
+                    }
                 });
+
+                $.getJSON(url, function (responese) {
+                    $.each(responese, function (index, user) {
+                        if(user.username==userID){
+                            $.each(user.projects, function (projectIndex,projectName) {
+                                //create new list item
+                                var project = $('<li/>');
+                                project.prop("class","project-element");
+
+                                //create new project link
+                                var project_link = $('<a/>',{href:"#"});
+
+                                //create new invite button for each project
+                                var invite_btn = $('<button/>',{type:"button" , class:"add-btn"});
+
+                                invite_btn.click(function () {
+                                    inviteForm.dialog("open");
+                                });
+
+                                invite_btn.addClass("btn btn-default");  //new class for styling
+                                invite_btn.text("+");
+
+                                //get project name
+                                var projectN = user.projects[projectIndex].project;
+
+                                //append name of each project to the project link
+                                project_link.text(projectN);
+
+                                //append the project link to the list item then append the invite button
+                                project.append(project_link);
+                                project.append(invite_btn);
+
+                                //append the list item after the search box
+                                $('.project-menu li:eq(1)').after(project);
+                            });
+                        }
+                    });
+                });
+
+                content.find('.dropdown-toggle').click(function () {
+                    content.find('.project-menu').slideToggle("fast");
+                    content.find('.search-result').hide();
+                });
+
+
+                //create new project POPUP
+                addProjectForm.dialog({
+                    autoOpen: false,
+                    show: {
+                        effect: "fade"
+                    },
+                    modal: true,
+                    draggable: false,
+                    fluid: true,
+                    resizable: false,
+                    hide: {
+                        effect: "fade"
+                    },
+                    Close: function () {
+                        $(this).dialog("close");
+                    },
+                    Continue: function () {
+                        $(this).dialog("close");
+                    }
+                });
+
+                addProjectBtn.click(function () {
+                    addProjectForm.dialog("open");
+                });
+
+                addProjectForm.submit(function (e) {
+                    e.preventDefault();
+
+                });
+
+                //search function
+                searchField.keyup(function(){
+                    var filter = $(this).val();
+                    var count = 0;
+                    var Exp = new RegExp(filter, "i");
+
+                    content.find(".dropdown-menu .project-element").each(function(){
+
+                        if ($(this).text().search(Exp) < 0) {
+                            $(this).fadeOut();
+                        }
+                        else {
+                            $(this).show();
+                            count++;
+                        }
+                    });
+                });
+
+                return content;
+            },
+
+            //all url observe goes here
+            observeURLS: function () {
+
+            },
+
+            //called when the page is in view
+            //you can fetch data and show them here on startup if you want
+            start: function () {
+                //access content
+                var content = this.getContent();
+
+                console.log("Header-Widget: called start");
+            },
+
+            exit: function(){
+                console.log("Header-Widget: called exit");
             }
         });
-    });
 
-    $('.dropdown-toggle').click(function () {
-        $('.project-menu').slideToggle("fast");
-        $('.search-result').hide();
-    });
-
-
-    //create new project POPUP
-    addProjectForm.dialog({
-        autoOpen: false,
-        show: {
-            effect: "fade"
-        },
-        modal: true,
-        draggable: false,
-        fluid: true,
-        resizable: false,
-        hide: {
-            effect: "fade"
-        },
-        Close: function () {
-            $(this).dialog("close");
-        },
-        Continue: function () {
-            $(this).dialog("close");
-        }
-    });
-
-    addProjectBtn.click(function () {
-        addProjectForm.dialog("open");
-    });
-
-    addProjectForm.submit(function (e) {
-        e.preventDefault();
-
-    });
-
-    //search function
-    searchField.keyup(function(){
-        var filter = $(this).val();
-        var count = 0;
-        var Exp = new RegExp(filter, "i");
-
-        $(".dropdown-menu .project-element").each(function(){
-
-            if ($(this).text().search(Exp) < 0) {
-                $(this).fadeOut();
-            }
-            else {
-                $(this).show();
-                count++;
-            }
-        });
-    });
-
-});
+    }
+);
