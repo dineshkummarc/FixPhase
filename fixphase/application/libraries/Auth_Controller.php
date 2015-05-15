3ca72f4e9bdddbc322114da85e4a92b193501286
Karim Tarabishy <tarabishy_2020@hotmai.com>
2015-05-15 14:36:57 +0200

    Add user api and authorization.

--- fixphase/application/libraries/Auth_Controller.php
+++ fixphase/application/libraries/Auth_Controller.php
@@ -12,12 +12,33 @@ class Auth_Controller extends REST_Controller{
 
     function __construct(){
         parent::__construct();
+        //if he isnot logged then redirect to login
         if(!$this->session->userdata('logged_in')){
             redirect('Login');
         }
+
+        //if this isnot an ajax call then send to SPA
         if (!$this->input->is_ajax_request()) {
-            echo $this->load->view('home_view.html', '', TRUE);
+            $this->load->view('home_view.html');
+            $this->output->_display();
             die();
+        }
+        //otherwise check tampering in user_id
+        else{
+            //try to find user id in request
+            $user_id = $this->get("user_id");
+            if(!$user_id)
+            {
+                $user_id = $this->post("user_id");
+                if(!$user_id)
+                    $user_id = $this->put("user_id");
+            }
+            //if found and it isnot like the on in sessionid then return an error
+            if($user_id && $user_id != $this->session->userdata("user_id")){
+                //send error, flush it and die
+                $this->response(array("error"=> array("id" => "4")), NULL, true);
+                die();
+            }
         }
     }
 
