e524384018428b8b0704b7072638378a4bd4eda1
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-16 21:23:01 +0530

    URL

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://local.rocket.co.in:8081/FixPhase/';
 
 /*
 |--------------------------------------------------------------------------
